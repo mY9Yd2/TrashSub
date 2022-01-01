f488045d7fe2f8b9e153858b5945d31715bfc3da
Kovács József Miklós <kovacsjozsef7u@gmail.com>
2022-01-01 20:51:58 +0100

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+## About  
+TrashSub: auto translate ASS dialogues with DeepL (free) API  
+You have to use your own API KEY:  
+Create `deepL.txt` file, put your private API KEY there and save it.
+Put your `foo.ass` file in the same dir, rename it to `input.ass`.
+**SLOW**
+```bash
+./main.py >> output.txt
+```
+The script is not too big, you can easily modify to your needs.
---
+++ main.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+#
+# MIT License
+#
+# Copyright (c) 2022 Kovács József Miklós <kovacsjozsef7u@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+import re
+import os
+import sys
+import json
+import urllib.request
+import urllib.parse
+
+def fix_text(text: str) -> str:
+    text = text.replace(r'\n', '')
+    text = text.replace(r'\N', '')
+    # remove { foobar foobar }
+    text = re.sub('{.*?}', '', text)
+    return text
+
+def read_deepL_API_key() -> str:
+    if not os.path.exists('deepL.txt'):
+        print('deepL.txt not exist')
+        sys.exit(1)
+    with open('deepL.txt', 'r') as f:
+        # TODO: check?
+        return f.readline().strip()
+
+def translate_deepL(text: str, deepL_API_key: str, target_lang: str) -> str:
+    headers = {
+        'User-Agent': 'TrashSub',
+        'Host': 'api-free.deepl.com'
+    }
+    data = {
+        'auth_key': deepL_API_key,
+        'text': text,
+        'target_lang': target_lang
+    }
+    data = urllib.parse.urlencode(data).encode()
+    req = urllib.request.Request('https://api-free.deepl.com/v2/translate?auth_key=' + deepL_API_key, headers=headers, data=data)
+    resp = urllib.request.urlopen(req).read()
+    resp = json.loads(resp)
+
+    return resp['translations'][0]['text']
+
+def main() -> None:
+    deepL_API_key = read_deepL_API_key()
+    target_lang = 'HU'
+
+    dialogues = []
+
+    # TODO: better solution?
+    filename = 'input.ass'
+    with open(filename, 'r') as f:
+        for line in f:
+            line = line.strip()
+            if line.startswith('Dialogue: '):
+                line = line.split(',')
+                line = ''.join(line[9:])
+                dialogues.append(line)
+
+    for index, dialogue in enumerate(dialogues):
+        d = fix_text(dialogue)
+
+        print(f'Line {index+1}')
+        print(d)
+        print(translate_deepL(d, deepL_API_key, target_lang))
+
+        # empty line
+        print()
+
+if __name__ == '__main__':
+    main()
